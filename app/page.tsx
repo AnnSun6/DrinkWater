a41896101086253e20befe2559b39c8a00dc1009
AnnSun <annfeng666@gmail.com>
2026-02-25 00:01:09 +0800

    feat:water intake log

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,6 @@
 "use client"
 import { supabase } from '@/lib/supabase'
-import {useState, useEffect, useRef, useCallback} from 'react'
+import {useState, useEffect, useRef, useCallback, useMemo} from 'react'
 import { useRouter } from 'next/navigation'
 import toast from 'react-hot-toast'
 
